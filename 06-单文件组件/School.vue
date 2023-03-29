6929c88a9fc55948a911725fe126447139544569
qingzzhou <345206028@qq.com>
2023-03-29 12:14:19 +0800

    test

---
+++ 06-单文件组件/School.vue
@@ -0,0 +1,30 @@
+<template lang="">
+<!-- 组件的结构 -->
+    <div>
+        <h2>学生姓名：{{name}}</h2>
+        <h2>学生年龄：{{age}}</h2>
+        <button @click="showName">点我提示学校名</button>
+    </div>
+</template>
+
+<script>
+// 组件交互相关的代码（数据、方法等）
+    export default {
+    name:'School',
+    data() {
+        return {
+            name:'尚硅谷',
+            address:'北京'
+        }
+    },
+    methods: {
+        showName() {
+            alert(this.name)
+        }
+    },
+};
+</script>
+
+<style lang="">
+/* 组件的样式 */
+</style>
---
+++ 06-单文件组件/Student.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>学生姓名：{{name}}</h2>
+        <h2>学生年龄：{{age}}</h2>
+        <!-- <button @click="showName">点我提示学校名</button> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data() {
+        return {
+            name:'王二',
+            age:19
+        }
+    },
+    methods: {
+        showName() {
+            alert(this.name)
+        }
+    },
+}
+</script>
